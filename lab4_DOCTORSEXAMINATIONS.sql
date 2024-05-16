cc97bb5ad494199425bcd4f231ab80e333ac9319
Coon_potaskun <35497588+YuriiKlim@users.noreply.github.com>
2024-05-16 21:19:10 +0300

    LAB4

---
+++ lab4_DOCTORSEXAMINATIONS.sql
@@ -0,0 +1,40 @@
+-- CREATE TABLE DOCTORSEXAMINATIONS (
+--     ID SERIAL PRIMARY KEY,
+--     DOCTOR_ID INT NOT NULL,
+--     EXAMINATION_ID INT NOT NULL,
+--     FOREIGN KEY (DOCTOR_ID) REFERENCES DOCTORS(ID),
+--     FOREIGN KEY (EXAMINATION_ID) REFERENCES EXAMINATIONS(ID)
+-- );
+
+-- Заповнення таблиці DoctorsExaminations
+INSERT INTO DOCTORSEXAMINATIONS (DOCTOR_ID, EXAMINATION_ID) VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 6),
+(7, 7),
+(8, 8),
+(9, 9),
+(10, 10),
+(1, 11),
+(2, 12),
+(3, 13),
+(4, 14),
+(5, 15),
+(6, 16),
+(7, 17),
+(8, 18),
+(9, 19),
+(10, 20),
+(1, 21),
+(2, 22),
+(3, 23),
+(4, 24),
+(5, 25),
+(6, 26),
+(7, 27),
+(8, 28),
+(9, 29),
+(10, 30);
